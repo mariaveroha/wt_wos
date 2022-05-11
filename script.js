25109769789e914f941c48fef1a81ff490f0b049
Vladislav <v.verokha@keep-calm.ru>
2022-05-11 22:29:56 +0300

    test json

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
-let serials = [
-    {'name': 'Чикатило'},
-    {'name': 'Чикаго'},
-];
-let search = document.getElementById('search');
-search.addEventListener('input', function () {
-    //this = search
-    let query = this.value.toLowerCase(); //чика
-    const data = new FormData();
-    data.append('q', query);
-    //fetch('http://d7mxgn3kr8v4.net/engine/ajax/search.php?q='+encodeURIComponent(query), {
-    fetch('http://d7mxgn3kr8v4.net/engine/ajax/search.php', {
-        method: 'POST',
-        body: data,
-        mode: 'no-cors'
-    })
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-    // serials.forEach(function (item) {
-    //     if (item.name.toLowerCase().includes(query)) {
-    //         console.log(item.name);
-    //     }
-    // })
+
+let api = {
+    data: [
+        {
+            title: 'Дознаватель',
+            year: 2022,
+            country: 'Великобритания',
+            img: 'img/dozn.png'
+        },
+        {
+            title: 'Голубоглазый',
+            year: 2021,
+            country: 'Канада',
+            img: 'img/blue.png'
+        }
+    ]
+};
+
+let innerHtml = '';
+api.data.forEach(function (item) {
+   innerHtml = innerHtml + '<div class="col-3"><div class="card my-4" style="width: 12rem;"><img src="'+item.img+'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title"></h5>' +
+       '<p class="card-text"> <b>'+item.title+'</b><br> '+item.year+' '+item.country+', Фантастика</p>' +
+       '<a href="#" class="btn btn-primary">Смотреть</a>' +
+       '</div>' +
+       '</div>' +
+       '</div>'
 });
+document.getElementById('main_wrapper').innerHTML = innerHtml;
+
+// let serials = [
+//     {'name': 'Чикатило'},
+//     {'name': 'Чикаго'},
+// ];
+// let search = document.getElementById('search');
+// search.addEventListener('input', function () {
+//     //this = search
+//     let query = this.value.toLowerCase(); //чика
+//     const data = new FormData();
+//     data.append('q', query);
+//     //fetch('http://d7mxgn3kr8v4.net/engine/ajax/search.php?q='+encodeURIComponent(query), {
+//     fetch('http://d7mxgn3kr8v4.net/engine/ajax/search.php', {
+//         method: 'POST',
+//         body: data,
+//         mode: 'no-cors'
+//     })
+//         .then(function (response) {
+//             return response.text();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+//     // serials.forEach(function (item) {
+//     //     if (item.name.toLowerCase().includes(query)) {
+//     //         console.log(item.name);
+//     //     }
+//     // })
+// });
 
 // fetch(that.getAttribute('formaction'), {
 //     headers: {
